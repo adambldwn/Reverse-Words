76b8de5cd7c12bd1d985be0b9b9d1fd808e1732d
efe <adambldwn110@gmail.com>
2020-10-09 10:36:33 +0300

    js

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#text');
+const button = document.querySelector('#button');
+const result = document.querySelector('#result-text');
+
+button.addEventListener('click', reverse);
+
+input.addEventListener('keyup', function enter(e){
+    if(e.keyCode ===13){
+        reverse();
+    }
+})
+
+function reverse(){
+
+    if(input.value == ""){
+        result.innerText= "Please write a sentence";
+    }
+    else{
+        var sentence = input.value.split(" ").reverse().join(" ");
+        result.innerText= sentence;
+
+        input.value="";
+        input.focus();
+
+    }
+    
+}
